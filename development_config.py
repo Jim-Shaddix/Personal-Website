5201d1eafb6745f2549878cc0bb4b7a13edf42de
Jim Shaddix <jimmy.shaddix2.0@gmail.com>
2019-10-13 19:48:38 -0600

    I forgot to add the these files in the last commit

--- development_config.py
+++ development_config.py
@@ -2,5 +2,4 @@ from config import Basic_Config
 
 class Config(Basic_Config):
 
-    DEBUG = True # this command doesn't seem to actually do anything ...
-                 # but it should turn the debugger on.
+    DEBUG = True # this variable turns the Flask Debugger on!
